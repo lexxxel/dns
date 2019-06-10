959ad33dae8edcce10248b4b33c182ba59bca06e
Alexander Pitzer <pitzera@gmx.de>
2019-06-10 18:51:06 +0200

    add rest

--- lexxxel.de.lua
+++ lexxxel.de.lua
@@ -5,3 +5,14 @@ cname("test.lexxxel.de", "1.1.1.1")
 cname("pages.lexxxel.de", "tmhm3azy0r4qvauz.myfritz.net")
 cname("*.pages.lexxxel.de", "tmhm3azy0r4qvauz.myfritz.net")
 
+cname("cloud.lexxxel.de", "tmhm3azy0r4qvauz.myfritz.net")
+cname("cloud.lexxxel.de", "tmhm3azy0r4qvauz.myfritz.net")
+cname("cloud.lexxxel.de", "tmhm3azy0r4qvauz.myfritz.net")
+
+cname("*.lexxxel.de", "tmhm3azy0r4qvauz.myfritz.net")
+
+mx("git.lexxxel.de", "mx00.udag.de", 10)
+mx("git.lexxxel.de", "mx01.udag.de", 20)
+
+mx("lexxxel.de", "smx00.udag.de", 10)
+mx("lexxxel.de", "smx01.udag.de", 20)
