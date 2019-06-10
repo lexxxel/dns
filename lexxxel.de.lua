6503b1fd662c793cbba59fa79e6fb29b64c6be24
Alexander Pitzer <pitzera@gmx.de>
2019-06-11 00:00:41 +0200

    remove all other entries

--- lexxxel.de.lua
+++ lexxxel.de.lua
@@ -1,18 +1,4 @@
 
 a("lexxxel.de", "92.210.132.41")
-cname("git.lexxxel.de", "tmhm3azy0r4qvauz.myfritz.net")
-a("test.lexxxel.de", "1.1.1.1")
-cname("test2.lexxxel.de", "google.de")
-
-cname("pages.lexxxel.de", "tmhm3azy0r4qvauz.myfritz.net")
-cname("*.pages.lexxxel.de", "tmhm3azy0r4qvauz.myfritz.net")
-
-cname("cloud.lexxxel.de", "tmhm3azy0r4qvauz.myfritz.net")
 
 cname("*.lexxxel.de", "tmhm3azy0r4qvauz.myfritz.net")
-
---mx("git.lexxxel.de", "mx00.udag.de", 10)
---mx("git.lexxxel.de", "mx01.udag.de", 20)
-
---mx("lexxxel.de", "smx00.udag.de", 10)
---mx("lexxxel.de", "smx01.udag.de", 20)
