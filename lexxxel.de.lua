4c64d2eb2cafd0c7301d193b7ce386f881c2488c
Alexander Pitzer <pitzera@gmx.de>
2019-06-10 18:47:48 +0200

    add more entries

--- lexxxel.de.lua
+++ lexxxel.de.lua
@@ -1,4 +1,7 @@
 
 a("lexxxel.de", "92.210.132.41")
 cname("git.lexxxel.de", "tmhm3azy0r4qvauz.myfritz.net")
+cname("test.lexxxel.de", "1.1.1.1")
+cname("pages.lexxxel.de", "tmhm3azy0r4qvauz.myfritz.net")
+cname("*.pages.lexxxel.de", "tmhm3azy0r4qvauz.myfritz.net")
 
