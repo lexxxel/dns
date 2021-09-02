e9330735ad05a7842b28f6f1afd6351da232dfa3
Alexander Petermann <apit@lexxxel.de>
2021-09-02 23:37:40 +0200

    DNS: 2021-09-02 23:37:39

--- lexxxel.de.lua
+++ lexxxel.de.lua
@@ -1,4 +1,4 @@
 a("lexxxel.de", "31.17.189.127")
 cname("*.lexxxel.de", "p1rmcfy9s5tbijab.myfritz.net")
-mx(_a, "mx00.udag.de", 0)
-mx(_a, "mx01.udag.de", 1)
+mx(_a, "mx00.udag.de", 10)
+mx(_a, "mx01.udag.de", 20)
