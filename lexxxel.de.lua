174ea17d0f061f4703f4b8b35b811cd823770d2a
Alexander Petermann <apit@lexxxel.de>
2022-02-07 19:33:08 +0100

    DNS: 2022-02-07 19:33:08

--- lexxxel.de.lua
+++ lexxxel.de.lua
@@ -1,6 +1,7 @@
 a("lexxxel.de", "31.16.51.27")
 a("git.lexxxel.de", "89.58.25.61")
 a("mattermost.lexxxel.de", "89.58.25.61")
+a("pages.lexxxel.de", "89.58.25.61")
 cname("*.lexxxel.de", "p1rmcfy9s5tbijab.myfritz.net")
 mx(_a, "mx00.udag.de", 10)
 mx(_a, "mx01.udag.de", 20)
