e9d87a00ac44222b8f3c91ef4310c15e2fb748cf
Alexander Petermann <apit@lexxxel.de>
2022-02-27 23:24:03 +0100

    test

--- lexxxel.de.lua
+++ lexxxel.de.lua
@@ -13,7 +13,7 @@ txt(_a, "Ko7oBlWssUn6P2i09VF3/Ok4fAjPIfbTL/XIZG9enf+3jOVpmSQRxMuYg6yN6oNbZ6Ytq/z
 
 txt(_a, "v=spf1 mx ~all", 180)
 
-txt(_a, "v=DMARC1; p=none; rua=mailto:abuse@lexxxel.de; ruf=mailto:abuse@lexxxel.de; sp=none; ri=86400", 180)
+txt(concat("_dmarc", _a), "v=DMARC1; p=none; rua=mailto:abuse@lexxxel.de; ruf=mailto:abuse@lexxxel.de; sp=none; ri=86400", 180)
 
 txt(concat("mail", _a), "v=DKIM1; h=sha256; k=rsa; ", 180)
 txt(concat("mail", _a), "p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2epBFWPIc9BtMELPVnX2hPjpZ0AMhRp4aSg+BvywnUV8asVeKsinloYo/QsBz2L1cg4IENz8FSPqwCgxdfvuHNtErO3gZ5UQ7AKi9Mf/mlg197f+w5I3YiRvJDMTOTXO8GX1psLypVwRHYHTptB7JYC4r0j6uvFxA65VQvAc1+2KCP4MBJ1T5NXiRbYPvrau+oAUJwM+qPGnRP", 180)
@@ -21,4 +21,3 @@ txt(concat("mail", _a), "Ko7oBlWssUn6P2i09VF3/Ok4fAjPIfbTL/XIZG9enf+3jOVpmSQRxMu
 
 txt(concat("mail", _a), "v=spf1 mx ~all", 180)
 
-txt(concat("mail", _a), "v=DMARC1; p=none; rua=mailto:abuse@lexxxel.de; ruf=mailto:abuse@lexxxel.de; sp=none; ri=86400", 180)
